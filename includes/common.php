197553550b06e246a47d89af43fde93f344e0d68
justinsearle <justin.searle@sympatico.ca>
2016-07-03 22:11:55 -0400

    added a common file for session_start and constants. also added database file required for all database connections.
    separate database credentials into another file; ignore files that contain sensitive information or that are unrelated to the source code.

---
+++ includes/common.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Author: Justin Searle
+ * Date: 7/3/2016
+ * Description: Require this on all pages where the session is needed
+ *              Also define any constants for the web page here.
+ */
+
+//start the session
+session_start();
+
+//examples of constants
+//define("IMAGE_DIR", "images/");
+
+//examples of authorization for the website
+//define("IS_LOGGED_IN", isset($_SESSION['username']));
+//define("IS_USER", (IS_LOGGED_IN && (int) $_SESSION['level'] >= 2));
+//define("IS_ADMIN", (IS_LOGGED_IN && (int) $_SESSION['level'] >= 3));
+//define("IS_SUPER_ADMIN", (IS_ADMIN && (int) $_SESSION['level'] === 4));
+
+//toggle the hiding of errors
+//error_reporting(E_ERROR | E_WARNING | E_NOTICE | E_PARSE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+//error_reporting(E_ERROR | E_PARSE);
+?>
---
+++ includes/database.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Author: Justin Searle
+ * Date: 7/3/2016
+ * Description: Require this file anywhere you need connect to the database and call "global $db;" to assign to this connection to the $db variable
+ */
+require_once("config.php");
+
+$db = new PDO("mysql:host=$theHost;dbname=$theDatabase", $theUser, $thePass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// example of database connection below
+
+// global $db;
+// $sql = 'SELECT * FROM my_table WHERE id = :id';
+// $result = $db->prepare($sql);
+// $result->bindParam(":id", $id);
+// if ($result->execute() && $result->errorCode() == 0 && $result->rowCount() > 0) {
+//      return $result->fetchAll(PDO::FETCH_ASSOC);
+// }
+
+?>
