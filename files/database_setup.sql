9fff161a001e2e449bec3f1a3104b2fa6237ffc3
justinsearle <justin.searle@sympatico.ca>
2016-07-03 22:11:55 -0400

    added a common file for session_start and constants. also added database file required for all database connections.

---
+++ files/database_setup.sql
@@ -0,0 +1,30 @@
+--
+-- Table structure for table `game_connections`
+--
+
+CREATE TABLE IF NOT EXISTS `game_connections` (
+`id` int(11) NOT NULL,
+  `session_id` varchar(100) NOT NULL,
+  `unique_code` int(4) NOT NULL,
+  `host_ip_address` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game_connections`
+--
+ALTER TABLE `game_connections`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `game_connections`
+--
+ALTER TABLE `game_connections`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
