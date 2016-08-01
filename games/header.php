95ab4e67bb3bef0b35901355f5f418fad670db26
Haden <HadenHiles@gmail.com>
2016-08-01 19:30:51 -0400

    added apple touch and favicon

--- games/header.php
+++ games/header.php
@@ -4,8 +4,23 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="Introducing Lollipop, a sweet new take on Android.">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
         <title><?php echo $pageTitle; ?></title>
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon-57x57.png" sizes="57x57" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-72x72.png" sizes="72x72" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-76x76.png" sizes="76x76" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-114x114.png" sizes="114x114" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-120x120.png" sizes="120x120" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-144x144.png" sizes="144x144" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-152x152.png" sizes="152x152" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" sizes="180x180" />
+        <!--Win8 tile-->
+        <meta name="msapplication-TileImage" content="/apple-touch-icon.png">
+        <meta name="msapplication-TileColor" content="#fff"/>
+        <meta name="application-name" content="Party Games" />
+        <!--Plain favicon-->
+        <link rel="icon" href="/favicon.ico" />
 
         <!-- Page styles -->
         <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.css" />
--- splash/header.php
+++ splash/header.php
@@ -24,6 +24,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scaleable=no">
         <title>Party Games</title>
 
+        <link rel="apple-touch-icon" href="/apple-touch-icon-57x57.png" sizes="57x57" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-72x72.png" sizes="72x72" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-76x76.png" sizes="76x76" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-114x114.png" sizes="114x114" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-120x120.png" sizes="120x120" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-144x144.png" sizes="144x144" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-152x152.png" sizes="152x152" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" sizes="180x180" />
+        <!--Win8 tile-->
+        <meta name="msapplication-TileImage" content="/apple-touch-icon.png">
+        <meta name="msapplication-TileColor" content="#fff"/>
+        <meta name="application-name" content="Party Games" />
+        <!--Plain favicon-->
+        <link rel="icon" href="/favicon.ico" />
+
         <!-- Page styles -->
         <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
