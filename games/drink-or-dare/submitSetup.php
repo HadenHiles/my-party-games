2be431617c393f3e85ea4d6bed0df86aaa56f5e8
Haden <HadenHiles@gmail.com>
2016-08-07 23:49:59 -0400

    fixed missing field in drink or dare start method; pass drink or dare construct a proper game id

--- games/drink-or-dare/submitSetup.php
+++ games/drink-or-dare/submitSetup.php
@@ -1,11 +1,15 @@
 <?php
 require_once("../../includes/class.GameSession.php");
 require_once("../../includes/class.User.php");
+require_once("class.DrinkOrDare.php");
 require_once("../../includes/common.php");
 require_once("../../includes/database.php");
 
 $game_field_values = $_SESSION['game_field_values'];
-
 unset($_SESSION['game_field_values']);
+
+$mysession = new GameSession(SESSION_ID, DEVICE_IP);
+$drinkOrDare = new DrinkOrDare($_SESSION['user']['code'], $_SESSION['user']['userid'], $game_field_values['rounds'], 1, $game_field_values['drinks_to_win']);
+$drinkOrDare->start();
 
 header("location: ../../lobby/");
