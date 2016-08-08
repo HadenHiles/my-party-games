890e50a5db4e5dfc8f8368aee3a12dc38a1ecca3
Haden <HadenHiles@gmail.com>
2016-08-07 23:33:16 -0400

    test change

--- games/drink-or-dare/submitSetup.php
+++ games/drink-or-dare/submitSetup.php
@@ -5,6 +5,7 @@ require_once("../../includes/common.php");
 require_once("../../includes/database.php");
 
 $game_field_values = $_SESSION['game_field_values'];
+
 unset($_SESSION['game_field_values']);
 
 header("location: ../../lobby/");
