bcd25cba8fb3d06e1e9a4647a5d7ef4a304ca4b7
Haden <HadenHiles@gmail.com>
2016-08-09 14:38:26 -0400

    delete game functionality - redirect hosts to splash, players to join

--- lobby/main.js
+++ lobby/main.js
@@ -10,9 +10,6 @@
         dialog.showModal();
         dialog.querySelector('.close').addEventListener('click', function() {
             dialog.close();
-            if(dialog.code == 1) {
-                location.reload();
-            }
         });
     }
 
