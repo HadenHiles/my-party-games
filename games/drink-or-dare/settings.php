eb6dc7189a0b76bdd23cecb001a5a168a5473b5f
Haden <HadenHiles@gmail.com>
2016-08-09 21:14:25 -0400

    increase stealth input sizes in game setup settings

--- games/drink-or-dare/settings.php
+++ games/drink-or-dare/settings.php
@@ -7,11 +7,11 @@ $drinkOrDare = $drinkOrDare[0];
 ?>
 <!-- Slider with Starting Value -->
 <label for="roundSlider">Rounds</label>
-<input type="number" class="stealth" name="rounds" id="roundsInput" value="<?php echo $drinkOrDare['total_rounds']; ?>" style="float: right; width: 35px; text-align: right;" readonly />
+<input type="number" class="stealth" name="rounds" id="roundsInput" value="<?php echo $drinkOrDare['total_rounds']; ?>" style="float: right; width: 60px; text-align: right;" readonly />
 <div class="clear"></div>
 <input class="mdl-slider mdl-js-slider" id="roundSlider" type="range" min="1" max="5" value="<?php echo $drinkOrDare['total_rounds']; ?>" step="1" />
 <label for="drinksToWinSlider">Drinks To Win</label>
-<input type="number" class="stealth" name="drinks_to_win" id="drinksToWinInput" value="<?php echo $drinkOrDare['drinks_to_win']; ?>" style="float: right; width: 35px; text-align: right;" readonly />
+<input type="number" class="stealth" name="drinks_to_win" id="drinksToWinInput" value="<?php echo $drinkOrDare['drinks_to_win']; ?>" style="float: right; width: 60px; text-align: right;" readonly />
 <div class="clear"></div>
 <input class="mdl-slider mdl-js-slider" id="drinksToWinSlider" type="range" min="5" max="25" value="<?php echo $drinkOrDare['drinks_to_win']; ?>" step="5" />
 
