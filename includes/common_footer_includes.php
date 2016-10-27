19b8a346908b3d5a2bae7889fbe1ab9351c2c9d0
Haden <HadenHiles@gmail.com>
2016-10-26 21:09:14 -0400

    remove hentai

--- includes/common_footer_includes.php
+++ includes/common_footer_includes.php
@@ -39,7 +39,6 @@ $(function() {
         "http://67.media.tumblr.com/4d92609b430dc10cc1dbe648b35aa7ad/tumblr_nmxi5gL0jL1sfb87ho1_500.png",
         "https://www.shareicon.net/horror-knife-terror-spooky-head-halloween-scary-costume-fear-828598/data/128x128/2016/10/18/845341_avatar_512x512.png",
         "https://www.shareicon.net/horror-knife-terror-spooky-head-halloween-scary-costume-fear-828598/data/128x128/2016/10/18/845346_avatar_512x512.png",
-        "http://pre15.deviantart.net/a283/th/pre/i/2012/305/e/6/erza_halloween_by_pervyangel-d5jnv2z.png",
         "https://s-media-cache-ak0.pinimg.com/originals/34/d5/62/34d5624c216ba9ac9c2510eb6f5923de.jpg",
         "https://holybeeofephesus.files.wordpress.com/2015/10/the_raven1.png",
         "http://i.imgur.com/lz7lLow.png",
@@ -76,11 +75,11 @@ $(function() {
     console.log(images.length);
     var inte = setInterval(function() {
         if (!toggle) {
-            op+=.05
+            op+=.02;
         } else {
-            op-=.1;
+            op-=.03;
         }
-        if (op >= .8) {
+        if (op >= .9) {
             toggle = true;
         } else if ( op <= 0) {
             toggle = false;
@@ -91,7 +90,7 @@ $(function() {
             $(ghost).css("left", Math.floor((Math.random() * 80) + 10)+"%");
         }
         $(ghost).css("opacity", op);
-    }, 100);
+    }, 50);
 });
 </script>
 
